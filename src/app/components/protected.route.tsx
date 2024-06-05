5cdcc1ff5adbc202dc37d5b1dd910c28d6779731
Marcus Bego Gomes <3407949+marcus-gomes-v@users.noreply.github.com>
2024-06-05 14:18:40 +0200

    fix: remove the /login and make the user back to the / (#19)
    Co-authored-by: M.V. Bego <marcus-gomes-v@users.noreply.github.com>

--- src/app/components/protected.route.tsx
+++ src/app/components/protected.route.tsx
@@ -10,7 +10,7 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     if (!loading && !authUser) {
-      router.push('/login');
+      router.push('/');
     }
   }, [authUser, loading, router]);
 
